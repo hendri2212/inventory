3d947d1d5b2bf99bdfb2169b1de451ed2e6e18ac
unknown <rieeriee219@gmail.com>
2023-08-29 14:17:18 +0800

    ketinggalan

--- resources/views/category/add.blade.php
+++ resources/views/category/add.blade.php
@@ -1,17 +1,11 @@
 @extends('index')
 
 @section('konten')
-tes tes tes
-    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt ipsum a, voluptas saepe praesentium, enim tempora ea et magnam commodi quia possimus nisi doloremque. Repellat nulla reiciendis nobis voluptatem neque.
-    Quasi culpa placeat deleniti distinctio cum praesentium expedita, voluptatem dolorem, ex nihil voluptates ullam, sapiente enim a incidunt. Deleniti provident omnis voluptas! Molestiae non repellendus facilis dolorum impedit assumenda temporibus.
-    Voluptatem quae laboriosam, odit tempore ipsum accusamus molestiae a inventore consectetur quisquam nulla suscipit! Quasi, officiis illo sapiente iste qui dolor ratione a quas odio cum nesciunt excepturi alias eius.
-    Vero maiores, nostrum, deserunt deleniti quas, nesciunt minima inventore incidunt beatae ex error neque voluptatibus laborum tenetur officia consequuntur. Corrupti doloremque ut ea unde dolor veritatis at nisi nobis. Optio.
-    Dolores est eveniet nihil, dicta excepturi enim repellendus saepe iure maiores quia et, nam eius quidem sapiente tempore optio rerum. Dignissimos atque alias, sapiente nisi blanditiis earum neque repellat aliquid?
-    <!-- <form action="{{url('category')}}" method="POST">
+    <form action="{{url('category')}}" method="POST">
         @csrf
-        <label for="name">Nama :</label>
-        <input type="text" name="name">
+        <label for="nama">Nama :</label>
+        <input type="text" name="nama">
         <br>
         <input type="submit" value="Simpan">
-    </form> -->
+    </form>
 @endsection
