36da9a7445108a40c8aa411bb31a7385dd59f685
unknown <rieeriee219@gmail.com>
2023-08-29 08:04:20 +0800

    Clean dikit

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -25,6 +25,9 @@
   <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
   <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+
+  <!-- @vite(['resources/css/app.css', 'resources/js/app.js']) -->
+  @vite(['resources/js/app.js'])
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
 <div class="wrapper">
@@ -51,6 +54,7 @@
 
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
+        
       <!-- Navbar Search -->
       <li class="nav-item">
         <a class="nav-link" data-widget="navbar-search" href="#" role="button">
@@ -73,102 +77,7 @@
         </div>
       </li>
 
-      <!-- Messages Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-comments"></i>
-          <span class="badge badge-danger navbar-badge">3</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="dist/img/user1-128x128.jpg" alt="User Avatar" class="img-size-50 mr-3 img-circle">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Brad Diesel
-                  <span class="float-right text-sm text-danger"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">Call me whenever you can...</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="dist/img/user8-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  John Pierce
-                  <span class="float-right text-sm text-muted"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">I got your message bro</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <!-- Message Start -->
-            <div class="media">
-              <img src="dist/img/user3-128x128.jpg" alt="User Avatar" class="img-size-50 img-circle mr-3">
-              <div class="media-body">
-                <h3 class="dropdown-item-title">
-                  Nora Silvester
-                  <span class="float-right text-sm text-warning"><i class="fas fa-star"></i></span>
-                </h3>
-                <p class="text-sm">The subject goes here</p>
-                <p class="text-sm text-muted"><i class="far fa-clock mr-1"></i> 4 Hours Ago</p>
-              </div>
-            </div>
-            <!-- Message End -->
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Messages</a>
-        </div>
-      </li>
-      <!-- Notifications Dropdown Menu -->
-      <li class="nav-item dropdown">
-        <a class="nav-link" data-toggle="dropdown" href="#">
-          <i class="far fa-bell"></i>
-          <span class="badge badge-warning navbar-badge">15</span>
-        </a>
-        <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-          <span class="dropdown-item dropdown-header">15 Notifications</span>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-envelope mr-2"></i> 4 new messages
-            <span class="float-right text-muted text-sm">3 mins</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-users mr-2"></i> 8 friend requests
-            <span class="float-right text-muted text-sm">12 hours</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item">
-            <i class="fas fa-file mr-2"></i> 3 new reports
-            <span class="float-right text-muted text-sm">2 days</span>
-          </a>
-          <div class="dropdown-divider"></div>
-          <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="fullscreen" href="#" role="button">
-          <i class="fas fa-expand-arrows-alt"></i>
-        </a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" data-widget="control-sidebar" data-controlsidebar-slide="true" href="#" role="button">
-          <i class="fas fa-th-large"></i>
-        </a>
-      </li>
-    </ul>
+
   </nav>
   <!-- /.navbar -->
 
@@ -206,85 +115,85 @@
 
       <!-- Sidebar Menu -->
       <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-               <li class="nav-item">
+
+        <li class="nav-item">
             <a href="/beranda" class="nav-link active">
-              <i class="nav-icon far fa-image"></i>
-              <p>
-                Beranda
-              </p>
+                <i class="nav-icon far fa-image"></i>
+                <p>
+                    Beranda
+                </p>
             </a>
-          </li>
-          <li class="nav-item">
-            <a href="/goods" href="/tlb" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Data Barang
-              </p>
+        </li>
+
+        <li class="nav-item">
+            <a href="/good" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                    Data Barang
+                </p>
             </a>
-          </li>
+        </li>
+
           <li class="nav-item">
             <a href="/transaction" class="nav-link">
-              <i class="nav-icon fas fa-table"></i>
-              <p>
-                Transaksi
-              </p>
+                <i class="nav-icon fas fa-table"></i>
+                <p>
+                    Transaksi
+                </p>
             </a>
-          </li>
-          <li class="nav-item">
-            <a href="/supply" class="nav-link">
-              <i class="nav-icon fas fa-edit"></i>
-              <p>
-                Supply
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/rooms" class="nav-link">
-              <i class="nav-icon fas fa-columns"></i>
-              <p>
-                Rooms
-              </p>
-            </a>
-          </li>
-          <li class="nav-item">
-            <a href="/category" href="/tlb" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
-              <p>
-                Kategory
-              </p>
-            </a>
-          </li>
-        </ul>
-      </nav>
-      <!-- /.sidebar-menu -->
-    </div>
-    <!-- /.sidebar -->
-  </aside>
+        </li>
 
-  <!-- Content Wrapper. Contains page content -->
-  @yield('tittle')
+        <li class="nav-item">
+            <a href="/supply" class="nav-link">
+                <i class="nav-icon fas fa-edit"></i>
+                <p>
+                    Supply
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="/rooms" class="nav-link">
+                <i class="nav-icon fas fa-columns"></i>
+                <p>
+                    Rooms
+                </p>
+            </a>
+        </li>
+
+        <li class="nav-item">
+            <a href="/category" href="/tlb" class="nav-link">
+                <i class="nav-icon fas fa-th"></i>
+                <p>
+                    Kategory
+                </p>
+            </a>
+        </li>
+    </ul>
+            </nav>
+        <!-- /.sidebar-menu -->
+    </div>
+
+</aside>
     <!-- /.content-header -->
+    @yield('tittle')
 
     <!-- Main content -->
     <section class="content">
+    
     @yield('konten')
      
     </section>
-    <!-- /.content -->
-  </div>
-  <!-- /.content-wrapper -->
- 
 
-  <!-- Control Sidebar -->
-  <aside class="control-sidebar control-sidebar-dark">
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
-  </aside>
-  <!-- /.control-sidebar -->
-</div>
-<!-- ./wrapper -->
+</aside>
+
+
 
 <!-- jQuery -->
 <script src="plugins/jquery/jquery.min.js"></script>
