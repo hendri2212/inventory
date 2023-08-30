77de9626d1422a29f9185bafd1d9ea7c5c47c449
unknown <rieeriee219@gmail.com>
2023-08-30 09:29:11 +0800

    memperbaiki navbar

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from 'path';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: ['resources/css/app.css', 'resources/js/app.js','dist/css/adminlte.min.css','plugins/icheck-bootstrap/icheck-bootstrap.min.css'],
             refresh: true,
         }),
     ],
